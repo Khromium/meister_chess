b5842df338efe913520b622bad143027b99d4616
khrom <devs@outlook.jp>
2019-02-19 16:22:39 +0900

    ganbatta

--- project/arm/chess_machine.py
+++ project/arm/chess_machine.py
@@ -1,6 +1,8 @@
+import asyncio
 import Adafruit_PCA9685
 import arm.arm_controler as ac
 from core.board import Board
+import chess.engine
 
 class ChessMachine:
     def __init__(self, board, arm):
@@ -26,11 +28,135 @@ class ChessMachine:
         except Exception as e:
             print(str(e))
     
+    def cpu_vs_cpu(self):
+        async def main():
+            transport, engine = await chess.engine.popen_uci("stockfish")
+
+            while not self.board.check_game_over():
+                cpu_result = await engine.play(self.board.board, chess.engine.Limit(time=0.100))
+                route = self.board.piece_move(cpu_result.move)
+                if len(route) != 1: # イリーガルムーブだとrouteに[-1]が入ってる
+                    for pos in route:
+                        self.arm.move_pos(pos)
+                    print(str(route))
+                    self.board.board.push(cpu_result.move)
+                    self.board.check_state()
+                else:
+                    print('正しく入力して下さい．終了するときはCtrl+C')
+                    continue
+                self.board.print_board()
+
+            await engine.quit()
+        
+        asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+        asyncio.run(main())
+    
+    def cpu_vs_human(self):
+        async def main():
+            transport, engine = await chess.engine.popen_uci("stockfish")
+
+            while not self.board.check_game_over():
+                while True:
+                    cpu_result = await engine.play(self.board.board, chess.engine.Limit(time=0.100))
+                    route = self.board.piece_move(cpu_result.move)
+                    if len(route) != 1: # イリーガルムーブだとrouteに[-1]が入ってる
+                        for pos in route:
+                            self.arm.move_pos(pos)
+                        print(str(route))
+                        self.board.board.push(cpu_result.move)
+                        self.board.check_state()
+                        break
+                    else:
+                        print('正しく入力して下さい．終了するときはCtrl+C')
+                        continue
+                    self.board.print_board()
+            
+                while True:
+                    move = input()
+                    route = self.board.piece_move_str(move)
+                    if len(route) != 1: # イリーガルムーブだとrouteに[-1]が入ってる
+                        for pos in route:
+                            self.arm.move_pos(pos)
+                        print(str(route))
+                        self.board.board.push_san(move)
+                        self.board.check_state()
+                        break
+                    else:
+                        print('正しく入力して下さい．終了するときはCtrl+C')
+                        continue
+
+            await engine.quit()
+        
+        asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+        asyncio.run(main())
+
+    def human_vs_cpu(self):
+        async def main():
+            transport, engine = await chess.engine.popen_uci("stockfish")
+
+            while not self.board.check_game_over():
+                while True:
+                    move = input()
+                    route = self.board.piece_move_str(move)
+                    if len(route) != 1: # イリーガルムーブだとrouteに[-1]が入ってる
+                        for pos in route:
+                            self.arm.move_pos(pos)
+                        print(str(route))
+                        self.board.board.push_san(move)
+                        self.board.check_state()
+                        break
+                    else:
+                        print('正しく入力して下さい．終了するときはCtrl+C')
+                        continue
+                
+                while True:
+                    cpu_result = await engine.play(self.board.board, chess.engine.Limit(time=0.100))
+                    route = self.board.piece_move(cpu_result.move)
+                    if len(route) != 1: # イリーガルムーブだとrouteに[-1]が入ってる
+                        for pos in route:
+                            self.arm.move_pos(pos)
+                        print(str(route))
+                        self.board.board.push(cpu_result.move)
+                        self.board.check_state()
+                        break
+                    else:
+                        print('正しく入力して下さい．終了するときはCtrl+C')
+                        continue
+                    self.board.print_board()
+
+            await engine.quit()
+        
+        asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+        asyncio.run(main())
+
+    def human_vs_human(self):
+        try:
+            while True:
+                move = input()
+                route = self.board.piece_move_str(move)
+                if len(route) != 1: # イリーガルムーブだとrouteに[-1]が入ってる
+                    for pos in route:
+                        self.arm.move_pos(pos)
+                    print(str(route))
+                    self.board.board.push_san(move)
+                    self.board.check_state()
+                else:
+                    print('正しく入力して下さい．終了するときはCtrl+C')
+                    continue
+            self.arm.home_pos()
+                
+        except KeyboardInterrupt  :  #Ctl+Cが押されたらループを終了
+            print("\nCtl+C")
+            self.arm.home_pos()
+
+        except Exception as e:
+            print(str(e))
+    
     def human_control(self):
         try:
             while True:
                 pos_str = input()
-                if len(pos_str) == 2 and pos_str[0] in 'abcdefgh': # 普通の指令のとき
+                if len(pos_str) == 2 and pos_str[0] in 'abcdefgh' and pos[1] in '12345678': # 普通の指令のとき
                     pos_str = pos_str.translate(str.maketrans({'a':'0','b':'1','c':'2','d':'3','e':'4','f':'5','g':'6','h':'7'}))
                     pos = int(pos_str[0]) + 8*(8 - int(pos_str[1]))
                 elif pos_str == 'x':
