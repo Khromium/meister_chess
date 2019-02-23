2e329dbeae51a5afed2602cf536f5a2478a04518
khrom <devs@outlook.jp>
2019-02-24 00:26:21 +0900

    add アームに処理を送信する部分を書いた。

--- project/assist/assistant.py
+++ project/assist/assistant.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import argparse
 import os.path
 import json
-
+import re
 import google.oauth2.credentials
 
 from google.assistant.library import Assistant
@@ -19,15 +19,19 @@ class Assist:
     # JSONファイルのパス
     filepath = "speech.json"
 
-    def __init__(self):
+    def __init__(self, board, arm):
         """
         初期化部分でJSONの初期化とGoogle assistantの有効化を行っている
         なお、認証時には
-        /.config/google-oauthlib-tool/credentials.json
+        ~/.config/google-oauthlib-tool/credentials.json
         に認証ファイルが必要
+        :param board:
+        :param arm:
         """
+        self.board = board
+        self.arm = arm
         # Jsonデータ初期化
-        self.initJson()
+        self._initJson()
         # GoogleAssistantSDK認証
         parser = argparse.ArgumentParser(
             formatter_class=argparse.RawTextHelpFormatter)
@@ -46,7 +50,42 @@ class Assist:
             self.credentials = google.oauth2.credentials.Credentials(token=None,
                                                                      **json.load(f))
 
-    def writeJson(self, path, data):
+    def _name_to_character(self, piece_with_str: str):
+        """
+        名前を文字列に修正
+        :param piece_with_str:
+        :return:
+        """
+        pairs = {"ルーク": "R", "ビショップ": "B", "クイーン": "Q", "ナイト": "N", "ポーン": "P", "キング": "K"}
+        for k, v in pairs.items():
+            if piece_with_str.count(k):
+                return v
+
+    def send_to_arm(self, query_txt):
+        """
+        アームに司令を送る関数
+        クエリのテキストから駒情報と移動先情報を取得して、送信する。
+        :param query_txt:
+        :return:
+        """
+        txt = re.sub(r"\n\s", "", query_txt, flags=(re.MULTILINE | re.DOTALL))
+        piece = self._name_to_character(txt)
+        field = re.search(r"[a-h][1-8]", txt)
+        print(piece + "を" + field + "へ動かします。")
+
+        san = piece + field
+        route = self.board.piece_move_str(san)
+        if len(route) != 1:  # イリーガルムーブだとrouteに[-1]が入ってる
+            for pos in route:
+                self.arm.move_pos(pos)
+            print(str(route))
+            self.board.board.push_san(san)
+            self.board.check_state()
+        else:
+            print('無効な移動です。')
+        self.arm.home_pos()
+
+    def _writeJson(path, data):
         """
         JSONファイルの書き込み
         :param path:
@@ -56,7 +95,7 @@ class Assist:
         with open(path, "w") as f:
             json.dump(data, f, indent=4)
 
-    def initJson(self):
+    def _initJson(self):
         """
         JSONデータ用初期化処理
         :return:
@@ -66,9 +105,9 @@ class Assist:
             "txtStatus": "waiting",
             "txt": "",
         }
-        self.writeJson(self.filepath, self.speechdata)
+        self._writeJson(self.filepath, self.speechdata)
 
-    def process_event(self, event):
+    def _process_event(self, event):
         """
         ここでGOOGLE HOMEとのやり取りをテキストとしてフックして
         必要な処理を加えていくことで処理を実現する。
@@ -87,23 +126,29 @@ class Assist:
             self.speechdata["status"] = "start"
             self.speechdata["txtStatus"] = "waiting"
             self.speechdata["txt"] = ""
-            self.writeJson(self.filepath, self.speechdata)
+            self._writeJson(self.filepath, self.speechdata)
 
         if event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED:
             print("ON_RECOGNIZING_SPEECH_FINISHED")
             print(event.args)
 
-            queryTxt = event.args["text"]
+            query_txt = event.args["text"]
             self.speechdata["status"] = "talking"
             self.speechdata["txtStatus"] = "query"
-            self.speechdata["txt"] = queryTxt
+            self.speechdata["txt"] = query_txt
 
-            self.writeJson(self.filepath, self.speechdata)
+            self._writeJson(self.filepath, self.speechdata)
 
         # 応答のテキスト取得
         if event.type == EventType.ON_RENDER_RESPONSE:
             print("ON_RENDER_RESPONSE")
             print(event.args)
+            query_txt = event.args["text"]
+            self.speechdata["status"] = "talking"
+            self.speechdata["txtStatus"] = "query"
+            self.speechdata["txt"] = query_txt
+            self._writeJson(self.filepath, self.speechdata)
+            self.send_to_arm(query_txt)
 
         if event.type == EventType.ON_RESPONDING_STARTED:
             print("ON_RESPONDING_STARTED")
@@ -121,19 +166,13 @@ class Assist:
             self.speechdata["txtStatus"] = "waiting"
             self.speechdata["txt"] = ""
 
-            self.writeJson(self.filepath, self.speechdata)
+            self._writeJson(self.filepath, self.speechdata)
 
-    def main(self):
+    def activate(self):
         """
         GOOGLE ASSISTANTを有効にして処理を行う部分
         :return:
         """
         with Assistant(self.credentials, "zairiki") as assistant:
             for event in assistant.start():
-                self.process_event(event)
-
-
-# import文でモジュールとしてインポートされた場合ではなく
-# コマンドラインから実行された時に「main()」を実行します。
-if __name__ == "__main__":
-    Assist().main()
+                self._process_event(event)
